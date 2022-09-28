b98d57197125cf47973d1cb148398551543326d0
endura0535 <dhrtnqls0535@naver.com>
2022-09-28 15:26:25 +0900

    Doubly Linked List

--- 2_linked_list/main.py
+++ 2_linked_list/main.py
@@ -1,4 +1,7 @@
 from slist import SList
+from dlist import DList
+
+'''
 if __name__ == '__main__':
     s = SList()
     s.insert_front('orange')
@@ -7,3 +10,14 @@ if __name__ == '__main__':
     s.insert_after('cherry', s.head)
     s.print_list()
     #print('cherry는 %d번째' % s.search('cherry'))
+'''
+
+if __name__ == '__main__':
+    d = DList()
+    d.insert_after('apple', d.head)
+    d.insert_before('orange', d.tail)
+    d.insert_before('cherry', d.tail)
+    d.insert_after('pear', d.head.next)
+    d.delete(d.head.next.next)
+    d.print_list()
+
